a866a143f8198eb3fab37d3a9389a59ed7018cf7
Robert Peters <r.peters96@me.com>
2021-12-03 11:53:46 +0100

    First test of skill
    Co-authored-by: DerHipler <DerHipler@users.noreply.github.com>
    Co-authored-by: Ennomnom <Ennomnom@users.noreply.github.com>

--- __init__.py
+++ __init__.py
@@ -7,9 +7,36 @@ class RobotController(MycroftSkill):
 
     @intent_file_handler('controller.robot.intent')
     def handle_controller_robot(self, message):
-        self.speak_dialog('controller.robot')
+        direction_type = message.get('direction.type')
+        if direction_type is not None:
+            self.move_direction(direction_type)
+        else:
+            self.speak_dialog('which.direction')
 
+    def converse(self, message):
+        return self.move_direction(message.data['utterances'][0])
+
+    def move_direction(self, direction):
+        if self.voc_match(direction, 'left'):
+            # move left
+            self.log.info("left")
+            return True
+        elif self.voc_match(direction, 'right'):
+            # move right
+            self.log.info("right")
+            return True
+        elif self.voc_match(direction, 'forward'):
+            # move forward
+            self.log.info("forward")
+            return True
+        elif self.voc_match(direction, 'backward'):
+            # move backward
+            self.log.info("backward")
+            return True 
+        else:
+            return False
 
 def create_skill():
     return RobotController()
+    
 
